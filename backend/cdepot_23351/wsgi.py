76bf4b7ec126118ca6cbc9542766eb5fc0c06b76
Crowdbotics <team@crowdbotics.com>
2020-12-14 04:56:53 +0000

    Update models

--- backend/cdepot_23351/wsgi.py
+++ backend/cdepot_23351/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdepot_23351.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cdepot_23351.settings")
 
 application = get_wsgi_application()
