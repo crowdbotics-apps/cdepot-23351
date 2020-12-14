15592a4ddafc36805735917ac8d82ae39a85df16
Crowdbotics <team@crowdbotics.com>
2020-12-14 02:31:16 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const cdepotAPI = axios.create({
-  baseURL: "https://cdepot-23351-prod.herokuapp.com/",
+  baseURL: "https://cdepot-23351.botics.co/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function api_v1_customtext_list(action) {
